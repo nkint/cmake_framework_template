c4eeceb27190bee5a231a3c973d07c1fbee905dc
nkint <nootropic.kint@gmail.com>
2013-10-09 18:25:58 +0200

    push/pull fast/forward problems..

--- cmake/Findfoo.cmake
+++ cmake/Findfoo.cmake
@@ -3,12 +3,9 @@ message(" === Findfoo.cmake")
 
 set(PACKAGE_NAME "foo")
 
-# here the sould be a check if it is already built or not..
-# with this line it is building in modules/foo/build
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../modules/${PACKAGE_NAME} 
                  ${CMAKE_CURRENT_LIST_DIR}/../modules/${PACKAGE_NAME}/build
 )
-
 include_directories(${CMAKE_CURRENT_LIST_DIR}/../modules/${PACKAGE_NAME}/include)
 
 set(LIB ${LIB} foo)
--- modules/foo/src/foo.cpp
+++ modules/foo/src/foo.cpp
@@ -1,7 +1,7 @@
-#include "foo.h"
+#include "foo/foo.h"
 
 #include <iostream>
 
 void Foo::foo() {
-	std::cout << "foo" << std::endl;
+    std::cout << "foo" << std::endl;
 }
